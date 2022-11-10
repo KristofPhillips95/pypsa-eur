d6f97afb69ce49976ec3b12159cd9a106a76f16e
Kristof Phillips <Kristof.phillips@kuleuven.be>
2022-11-10 15:12:57 +0100

    Rewable shapes adapted

---
+++ scripts/build_renewable_shapes_iteration_v2.py
@@ -0,0 +1,171 @@
+import renewable_shapes_methods as rsm
+import numpy as np
+import geopandas as gpd
+from _helpers import configure_logging
+import os
+from joblib import Parallel,delayed
+import time
+
+
+def solve_model_and_save_to_dict(seed, model, dist, gdfs, bl_dict):
+    #print(seed)
+    np.random.seed(seed)
+    model.solve()
+    dist[seed] = dict()
+    for label in model.labels_:
+        dist[seed][label] = ((model.data[model.labels_ == label] - model.centroids_[label]) ** 2)
+    gdfs[seed] = rsm.create_geodataframe_with_shapes_from_k_means_model(lat_all_gran, lon_all_gran, model)
+    bl_dict[seed] = rsm.get_nb_buddies_list(model)
+
+def solve_model_and_return(seed, model):
+    #print(seed)
+    np.random.seed(seed)
+    model.solve()
+    dist = dict()
+    for label in model.labels_:
+        dist[label] = ((model.data[model.labels_ == label] - model.centroids_[label]) ** 2)
+    gdf = rsm.create_geodataframe_with_shapes_from_k_means_model(lat_all_gran, lon_all_gran, model)
+    bl = rsm.get_nb_buddies_list(model)
+    return seed,dist,gdf,bl,model.labels_,model.centroids_
+
+def unpack_and_assign_to_dicts(list,dist,gdfs,bl_dict,labels_dict,centroids_dict):
+    for tuple in list:
+        seed = tuple[0]
+        dist[seed] = tuple[1]
+        gdfs[seed] = tuple[2]
+        bl_dict[seed] = tuple[3]
+        labels_dict[seed] = tuple[4]
+        centroids_dict[seed] = tuple[5]
+
+
+if __name__ == "__main__":
+    if 'snakemake' not in globals():
+        from _helpers import mock_snakemake
+        snakemake = mock_snakemake('build_renewable_shapes',regions = 's-20')
+    configure_logging(snakemake)
+    save_fig = snakemake.config["renewable_zones"]["save_fig"]
+    mainland_only = snakemake.config["renewable_zones"]["mainland_only"]
+    plot_selection_attr = snakemake.config["renewable_zones"]["plot_selection_attr"]
+    loc_weight = snakemake.config["renewable_zones"]["loc_weight"]
+    nb_iterations = snakemake.config["renewable_zones"]["nb_iterations"]
+    parallel = snakemake.config["renewable_zones"]["parallel"]
+
+    #Load datasets of era and sara into dict
+    ds = rsm.load_data_sets(snakemake.input)
+
+
+    #Extract main parameters of clustering method from config file and wildcard
+    gran = snakemake.config["renewable_zones"]["granularity"]
+    attr_wc = snakemake.wildcards.regions.partition("-")[0]
+    print(attr_wc)
+    print(snakemake.wildcards.regions.partition("-")[2])
+    if snakemake.wildcards.regions == "c":
+        nb_regions = 1
+        clusters = gpd.read_file(snakemake.input.country_shapes)
+    else:
+        nb_regions = int(snakemake.wildcards.regions.partition("-")[2])
+
+        nb_regions = int(snakemake.wildcards.regions.partition("-")[2])
+
+        # Name of the relevant data in datasets
+        name = {'w': 'wnd100m', 's': 'influx_direct'}
+        attr_1 = 'w'
+        attr_2 = 's'
+
+        # Get latitudes and longitudes from the dataset
+        lat, lon = rsm.get_lons_lats_from_ds(ds)
+        # And select the ones that occur in both datasets
+        sel_lat, sel_lon = rsm.intersect_lats_lons(lat, lon)
+
+        l1, l2 = rsm.get_rescaled_lats_and_lons(lat, lon, sel_lat, sel_lon, gran, attr_wc)
+
+        at_1, pu = rsm.change_data_granularity(
+            np.mean(ds[attr_1][name[attr_1]][:, :, :].data[:, sel_lat[attr_1], :][:, :, sel_lon[attr_1]],
+                    axis=0), lat[attr_1][sel_lat[attr_1]], lon[attr_1][sel_lon[attr_1]], l1, l2, gran)
+        at_2, pu = rsm.change_data_granularity(
+            np.mean(ds[attr_2][name[attr_2]][:, :, :].data[:, sel_lat[attr_2], :][:, :, sel_lon[attr_2]],
+                    axis=0), lat[attr_2][sel_lat[attr_2]], lon[attr_2][sel_lon[attr_2]], l1, l2, gran)
+
+        # Vector of repeated lats and lons
+        lat_all_gran = np.tile(l1, len(l2))
+        lon_all_gran = np.repeat(l2, len(l1))
+
+        # Create the actual regional k-means model to be solved
+        if attr_wc == 'ws':
+            model,data,selection = rsm.generate_regional_kmeans_model_locs(lon_all_gran, lat_all_gran, [at_1, at_2,lat_all_gran,lon_all_gran],loc_weight, nb_regions)
+        elif attr_wc == 'w':
+            model,data,selection = rsm.generate_regional_kmeans_model_locs(lon_all_gran, lat_all_gran, [at_1,lat_all_gran,lon_all_gran],loc_weight, nb_regions)
+        elif attr_wc == 's':
+            model,data,selection = rsm.generate_regional_kmeans_model_locs(lon_all_gran, lat_all_gran, [at_2,lat_all_gran,lon_all_gran],loc_weight, nb_regions)
+
+
+        # And solve it. Seed is given for reproducibility
+        np.random.seed(snakemake.config["renewable_zones"]["seed"])
+        distance = dict()
+        gdfs = dict()
+        bl_dict = dict()
+        dist = dict()
+        labels_dict = dict()
+        centroids_dict = dict()
+        seeds = np.random.randint(0, 1e9, nb_iterations)
+
+        tick = time.time()
+        if parallel:
+            tuples_list = Parallel(n_jobs=-1)(delayed(solve_model_and_return)(seed,model) for seed in seeds)
+            unpack_and_assign_to_dicts(tuples_list,dist,gdfs,bl_dict,labels_dict,centroids_dict)
+            tock = time.time()
+            print(f"Time spent in parallel solve equals:" , tock-tick )
+
+        else:
+            for seed in seeds:
+               solve_model_and_save_to_dict(seed,model,dist,gdfs,bl_dict)
+            tock = time.time()
+            print(f"Time spent in sequential solve equals:", tock - tick)
+
+
+        dist_per_att = dict()
+        dist_per_seed = dict()
+        bl_sums = dict()
+        circumference = dict()
+        for seed in dist:
+            bl_sums[seed] = dict()
+            d_per_at = (np.array([dist[seed][c_label].sum(axis=0) for c_label in dist[seed]]).sum(axis=0))
+            dist_per_att[seed] = d_per_at
+            dist_per_seed[seed] = d_per_at.sum()
+            circumference[seed] = (4 - bl_dict[seed]).sum()
+            for n in range(1, 5):
+                bl_sums[seed][n] = np.count_nonzero(bl_dict[seed] == n)
+
+        path_tosave_to = os.path.join(os.path.dirname(os.path.dirname(snakemake.output.renewable_shapes)), "results" , f"gdfs_{loc_weight}_{parallel}")
+        circ_min = float("inf")
+        seed_min = 0
+        for seed in gdfs:
+            gdfs[seed]["dist_tot"] = dist_per_seed[seed]
+            gdfs[seed]["circumference"] = circumference[seed]
+            gdfs[seed]["total_time"] = tock-tick
+
+            if circumference[seed] < circ_min:
+                circ_min = circumference[seed]
+                seed_min = seed
+
+            for at in range(len(dist_per_att[seed])):
+                gdfs[seed][f"dist_per_{at}"] = dist_per_att[seed][at]
+
+            #gdfs[seed]["dist_per_att"] = dist_per_att[seed]
+
+
+            if not(os.path.exists(path_tosave_to)):
+                os.makedirs(path_tosave_to)
+            # if not(os.path.exists(f"../results/gdfs")):
+            #     os.makedirs(f"../results/gdfs")
+            rsm.save_to_geojson(gdfs[seed],os.path.join(path_tosave_to,f"ren_clust_{snakemake.wildcards.regions}_{gran}_{seed}"))
+
+        rsm.plot_regions_scatter_gran_and_save(lat_all_gran, lon_all_gran, model,labels_dict[seed_min], centroids_dict[seed_min],seed,snakemake.output,gran)
+        rsm.save_to_geojson(gdfs[seed_min],snakemake.output.renewable_shapes)
+
+
+
+
+
+
+
---
+++ scripts/renewable_shapes_methods.py
@@ -0,0 +1,365 @@
+import geoplot
+from shapely.ops import unary_union
+import os
+import netCDF4 as nc
+import matplotlib.pyplot as plt
+import numpy as np
+import alphashape
+import geopandas as gpd
+import geopandas
+import libpysal
+from spopt.region import RegionKMeansHeuristic
+import shapely
+import pandas as pd
+
+def save_to_geojson(df, fn):
+    if os.path.exists(fn):
+        # os.unlink(fn)
+        pass
+    if not isinstance(df, gpd.GeoDataFrame):
+        df = gpd.GeoDataFrame(dict(geometry=df))
+    df = df.reset_index()
+    schema = {**gpd.io.file.infer_schema(df), 'geometry': 'Unknown'}
+    df.to_file(fn, driver='GeoJSON', schema=schema)
+
+def mini_shapes(lat_all, lon_all, coords=False):
+    shapes = []
+    shape_coords_list = []
+    sz_lat = max(lat_all[1:] - lat_all[0:-1])
+    sz_lon = max(lon_all[1:] - lon_all[0:-1])
+    # print(sz_lat)
+    # print(sz_lon)
+    alpha = 0.5
+
+    for lat, lon in zip(lat_all, lon_all):
+        shape_coords = [(round(lon + incr_lon * sz_lon / 2, 5), round(lat + incr_lat * sz_lat / 2, 5)) for incr_lat in
+                        (0, 1, -1) for incr_lon in (0, 1, -1)
+                        if not (incr_lon == incr_lat == 0)]
+        if not (coords):
+            shapes.append(alphashape.alphashape(shape_coords, alpha=alpha))
+
+        shape_coords_list.append(shape_coords)
+
+    if coords:
+        return shape_coords_list
+
+    return shapes
+
+def get_rescaled_lats_and_lons(lats,lons,sel_lat,sel_lon,new_sz,attr_wc):
+
+    def check_gran_larger_than_size(lats_,lons_,new_sz):
+        sz_lat = round(max(lats_[1:] - lats_[0:-1]), 5)
+        sz_lon = round(max(lons_[1:] - lons_[0:-1]), 5)
+        assert round(sz_lat, 5) == round(sz_lon, 5) <= new_sz
+
+
+
+    if len(attr_wc) ==1:
+        lats_ = np.round(lats[attr_wc][sel_lat[attr_wc]], 5)
+        lons_ = np.round(lons[attr_wc][sel_lon[attr_wc]], 5)
+        check_gran_larger_than_size(lats_,lons_,new_sz)
+        new_lats = [l for l in np.arange(min(lats_) + new_sz / 2, max(lats_) - new_sz / 2, new_sz)]
+        new_lons = [l for l in np.arange(min(lons_) + new_sz / 2, max(lons_) - new_sz / 2, new_sz)]
+
+    elif len(attr_wc)==2:
+        min_lat,max_lat,min_lon,max_lon = -180,180,-180,180
+        lats_,lons_ = dict(),dict()
+        for at in attr_wc:
+            lats_[at] = np.round(lats[at][sel_lat[at]], 5)
+            lons_[at] = np.round(lons[at][sel_lon[at]], 5)
+
+            min_lat = max(min_lat,min(lats_[at]))
+            max_lat = min(max_lat, max(lats_[at]))
+
+            min_lon = max(min_lon,min(lons_[at]))
+            max_lon = min(max_lon, max(lons_[at]))
+
+            check_gran_larger_than_size(lats_[at],lons_[at],new_sz)
+
+        new_lats = [l for l in np.arange(min_lat + new_sz / 2, max_lat - new_sz / 2, new_sz)]
+        new_lons = [l for l in np.arange(min_lon + new_sz / 2, max_lon - new_sz / 2, new_sz)]
+
+
+
+    return new_lats,new_lons
+
+def change_data_granularity(attr,lats,lons, new_lats, new_lons, new_sz):
+
+    pointsused = 0
+
+    assert attr.shape == (len(lats), len(lons))
+
+
+
+    attr_new = np.zeros((len(new_lats), len(new_lons)))
+
+    for new_lat in new_lats:
+        # Let's start by getting all the indices of old latitudes that that are now linked to this new lat.
+        # Assumption of symmetry around new lat
+        lat_range = (new_lat - new_sz / 2, new_lat + new_sz / 2)
+
+        indices_in_a = np.where(np.logical_and(lats >= lat_range[0], lats < lat_range[1]))[0]
+        # indices_border_a = np.where(np.logical_and(lats==lat_range[0], lats==lat_range[1]))
+
+        for new_lon in new_lons:
+            lon_range = (new_lon - new_sz / 2, new_lon + new_sz / 2)
+
+            indices_in_o = np.where(np.logical_and(lons >= lon_range[0], lons < lon_range[1]))[0]
+            # indices_border_o = np.where(np.logical_and(lons==lon_range[0], lons==lon_range[1]))
+
+            list_points_in = [attr[x, y] for x in indices_in_a for y in indices_in_o]
+            new_value = np.mean(list_points_in)
+            pointsused += len(list_points_in)
+            attr_new[np.where(new_lats == new_lat)[0], np.where(new_lons == new_lon)[0]] = new_value
+
+    return attr_new, pointsused
+
+# 
+# def change_granularity(attr, lats, lons, new_sz):
+#     ##To avoid numerical problems, let's first round the original coordinates
+#     lats = np.round(lats, 5)
+#     lons = np.round(lons, 5)
+# 
+#     pointsused = 0
+#     sz_lat = round(max(lats[1:] - lats[0:-1]), 5)
+#     sz_lon = round(max(lons[1:] - lons[0:-1]), 5)
+# 
+#     assert round(sz_lat, 5) == round(sz_lon, 5) <= new_sz
+# 
+#     assert attr.shape == (len(lats), len(lons))
+# 
+#     new_lats = [l for l in np.arange(min(lats) + new_sz / 2, max(lats) + new_sz / 2, new_sz)]
+#     new_lons = [l for l in np.arange(min(lons) + new_sz / 2, max(lons) + new_sz / 2, new_sz)]
+# 
+#     attr_new = np.zeros((len(new_lats), len(new_lons)))
+# 
+#     for new_lat in new_lats:
+#         # Let's start by getting all the indices of old latitudes that that are now linked to this new lat.
+#         # Assumption of symmetry around new lat
+#         lat_range = (new_lat - new_sz / 2, new_lat + new_sz / 2)
+# 
+#         indices_in_a = np.where(np.logical_and(lats >= lat_range[0], lats < lat_range[1]))[0]
+#         # indices_border_a = np.where(np.logical_and(lats==lat_range[0], lats==lat_range[1]))
+# 
+#         for new_lon in new_lons:
+#             lon_range = (new_lon - new_sz / 2, new_lon + new_sz / 2)
+# 
+#             indices_in_o = np.where(np.logical_and(lons >= lon_range[0], lons < lon_range[1]))[0]
+#             # indices_border_o = np.where(np.logical_and(lons==lon_range[0], lons==lon_range[1]))
+# 
+#             list_points_in = [attr[x, y] for x in indices_in_a for y in indices_in_o]
+#             new_value = np.mean(list_points_in)
+#             pointsused += len(list_points_in)
+#             attr_new[np.where(new_lats == new_lat)[0], np.where(new_lons == new_lon)[0]] = new_value
+# 
+#     return new_lons, new_lats, attr_new, pointsused
+
+def get_lons_lats_from_ds(ds):
+    lat = dict()
+    lon = dict()
+    start = dict()
+    end = dict()
+    for attr in ds:
+        lat[attr] = ds[attr]['lat']
+        lon[attr] = ds[attr]['lon']
+
+        start[attr] = 1
+        end[attr] = len(lat[attr]) * len(lon[attr])
+    return lat, lon
+
+def intersect_lats_lons(lat, lon):
+    b_lat = min(max(lat['s'][:]), max(lat['w'][:]),np.float64(65.))
+    b_lon = min(max(lon['s'][:]), max(lon['w'][:]),np.float64(35.))
+
+    # sel_lat = lat[attr_1] <= b_lat
+    # sel_lon = lon[attr_2] <= b_lon
+
+    sel_lat = dict()
+    sel_lon = dict()
+
+    sel_lat["s"] = lat["s"] <= b_lat
+    sel_lon["s"] = lon["s"] <= b_lon
+
+    sel_lat["w"] = lat["w"] <= b_lat
+    sel_lon["w"] = lon["w"] <= b_lon
+
+
+    return sel_lat,sel_lon
+
+def get_selection_list(lon_all_gran,lat_all_gran,mainland_only= False):
+    if mainland_only:
+        europe_land = gpd.read_file("../resources/regions_onshore.geojson")
+        eu_land = shapely.ops.unary_union(europe_land["geometry"])
+        all_points_gran = [shapely.geometry.Point((lon, lat)) for lon, lat in zip(lon_all_gran, lat_all_gran)]
+        selection = [np.any([point.within(geom) for geom in eu_land]) for point in all_points_gran]
+    else:
+        selection = list(np.repeat(True,len(lat_all_gran)))
+
+    return selection
+
+def generate_regional_kmeans_model(lon_all_gran, lat_all_gran, attributes,nb_regions,mainland_only =False):
+
+
+    selection = get_selection_list(lon_all_gran,lat_all_gran,mainland_only)
+
+    data = np.zeros(shape=(len(lon_all_gran), len(attributes)))[selection]
+
+    for att_index in range(len(attributes)):
+        data[:, att_index] = attributes[att_index].flatten('F')[selection]
+
+    data /= np.max(np.abs(data), axis=0)
+
+    gdf = create_geodataframe_mini_shapes(lat_all_gran,lon_all_gran,data,selection)
+    # gdf.plot(column = "values_1")
+    # plt.show()
+    w = libpysal.weights.Rook.from_dataframe(gdf)
+    model = RegionKMeansHeuristic(data, nb_regions, w)
+    return model,data,selection
+
+def generate_regional_kmeans_model_locs(lon_all_gran, lat_all_gran, attributes,weight,nb_regions,mainland_only =False):
+
+
+    selection = get_selection_list(lon_all_gran,lat_all_gran,mainland_only)
+
+    data = np.zeros(shape=(len(lon_all_gran), len(attributes)))[selection]
+
+    for att_index in range(len(attributes)):
+        data[:, att_index] = attributes[att_index].flatten('F')[selection]
+
+    data /= np.max(np.abs(data), axis=0)
+    if weight == 0:
+        data = data[:,:-2]
+    else:
+        data[:,-2:] *= weight
+
+    gdf = create_geodataframe_mini_shapes(lat_all_gran,lon_all_gran,data,selection)
+
+    w = libpysal.weights.Rook.from_dataframe(gdf)
+    model = RegionKMeansHeuristic(data, nb_regions, w)
+    return model,data,selection
+
+def create_geodataframe_mini_shapes(lat_all_gran,lon_all_gran,data,selection):
+    mini_shapes_gran = np.array(mini_shapes(lat_all_gran, lon_all_gran))
+    mini_shapes_gran = mini_shapes_gran[selection]
+
+    columns = [f"values_{i}" for i in range(data.shape[1])]
+    df_data = pd.DataFrame(columns=columns,data=data)
+    gdf = gpd.GeoDataFrame(geometry=mini_shapes_gran,data=df_data)
+
+    return gdf
+
+def plot_regions_scatter_and_save(lat_all, lon_all, ds,attributes,nb_regions,seed):
+    fig, axs = plt.subplots(1, len(attributes) + 1 , figsize=(12, 4))
+
+    colors = [model.centroids_[label, 0] for label in model.labels_]
+    axs[0].scatter(lon_all_gran, lat_all_gran, c=colors)
+    axs[0].set_title(f'K-means clusters {attributes}')
+
+    for attr_idx in range(len(attributes)):
+
+        axs[attr_idx+1].scatter(lon_all[attributes[attr_idx]], lat_all[attributes[attr_idx]],
+                        c=(np.mean(ds[attributes[attr_idx]][name[attributes[attr_idx]]][:, :, :].data, axis=0).flatten('F')))
+        axs[attr_idx+1].set_title(f'Input data mean {attributes[attr_idx]}')
+
+
+    fig.suptitle(f'Clustering {attributes[attr_idx]} {nb_regions}')
+    dir_path = os.path.join(snakemake.output.renewable_shapes.partition("resources")[0], "results", "plots")
+    path = os.path.join(dir_path, f"renewable_clusters_scatter_{attributes}_{nb_regions}_seed{seed}")
+    print(path)
+
+    isExist = os.path.exists(dir_path)
+    if not isExist:
+        # Create a new directory because it does not exist
+        os.makedirs(dir_path)
+    plt.savefig(path)
+
+def plot_regions_scatter_gran_and_save(lat_all_gran, lon_all_gran, model,labels,centroids,seed,snakemake_output,gran):
+    data = model.data
+    fig, axs = plt.subplots(1,data.shape[1] + 1 , figsize=(12, 4))
+    nb_regions = len(np.unique(labels))
+
+    colors = [centroids[label, 0] for label in labels]
+    axs[0].scatter(lon_all_gran, lat_all_gran, c=colors)
+    axs[0].set_title(f'K-means clusters')
+
+    for attr_idx in range(data.shape[1]):
+
+        axs[attr_idx+1].scatter(lon_all_gran, lat_all_gran,
+                        c=(data[:,attr_idx]))
+        axs[attr_idx+1].set_title(f'Input data mean {attr_idx} ' )
+
+
+    #fig.suptitle(f'Clustering {attributes[attr_idx]} {nb_regions}')
+    dir_path = os.path.join(snakemake_output.renewable_shapes.partition("resources")[0], "results", "plots")
+    path = os.path.join(dir_path, f"renewable_clusters_scatter_{nb_regions}_{gran}_seed{seed}")
+    print(path)
+
+    isExist = os.path.exists(dir_path)
+    if not isExist:
+        # Create a new directory because it does not exist
+        os.makedirs(dir_path)
+    plt.savefig(path)
+
+def plot_selection(data_selection,selection):
+    fig, axs = plt.subplots(1, data_selection.shape[1])
+
+    for attr_idx in range(data_selection.shape[1]):
+        if data_selection.shape[1] == 1:
+            axs.scatter(lon_all_gran[selection], lat_all_gran[selection], c=data_selection[:, attr_idx])
+        else:
+            axs[attr_idx].scatter(lon_all_gran[selection], lat_all_gran[selection], c=data_selection[:, attr_idx])
+
+    dir_path = os.path.join(snakemake.output.renewable_shapes.partition("resources")[0], "results", "plots","clustering_selection")
+    path = os.path.join(dir_path, f"renewable_clusters_scatter_{attr_wc}_{mainland_only}")
+    print(path)
+
+    isExist = os.path.exists(dir_path)
+    if not isExist:
+        # Create a new directory because it does not exist
+        os.makedirs(dir_path)
+    plt.savefig(path)
+
+def create_geodataframe_with_shapes_from_k_means_model(lat_all_gran,lon_all_gran,model):
+    clusters = geopandas.GeoDataFrame()
+
+    mini_shapes_attr_gran = np.array(mini_shapes(lat_all_gran, lon_all_gran))
+
+    for cluster in np.unique(model.labels_):
+        idx = (model.labels_ == cluster)
+        polygons = mini_shapes_attr_gran[idx]
+        cluster_shape = shapely.ops.unary_union(polygons)
+
+
+        frame_row = geopandas.GeoDataFrame(
+            {"name": [f"Ren_{cluster}"], "geometry": cluster_shape, "Cluster": cluster})
+        i=0
+        for center in model.centroids_[cluster]:
+            i+=1
+            frame_row[f"center{i}"]= center
+
+        #clusters = clusters.append(frame_row)
+        clusters = pd.concat([clusters,frame_row],axis=0,ignore_index=True)
+    return clusters
+
+def load_data_sets(input):
+    ds = dict()
+    ds["s"] = nc.Dataset(input.cutout_solar)
+    ds["w"] = nc.Dataset(input.cutout_wind)
+    return ds
+
+def get_number_of_buddies(point, model):
+    cluster = model.labels_[point]
+    # buddies = []
+    nb_buddies = 0
+    for neighbor in model.w.neighbors[point]:
+        # buddies.append[neighbor]
+        if model.labels_[neighbor] == cluster:
+            nb_buddies += 1
+    return nb_buddies
+
+def get_nb_buddies_list(model):
+    nb_buddy_list = []
+    for point in range(len(model.data)):
+        nb_buddy_list.append(get_number_of_buddies(point,model))
+    return np.array(nb_buddy_list)
+
